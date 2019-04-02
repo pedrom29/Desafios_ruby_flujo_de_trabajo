76e0f7f14ef43e815209e83940b253ef0ac6064f
Pedro <pedromatu29@gmail.com>
2019-04-01 21:31:53 -0300

    segundo_commit

--- emprendedor2.rb
+++ emprendedor2.rb
@@ -1,7 +1,7 @@
 
 
 
-  ut = (ARGV[0].to_i * ARGV[1].to_i) + ((ARGV[0].to_i*2) * ARGV[2].to_i) + ((ARGV[0].to_i*0) * ARGV[3].to_i) - ARGV[4].to_i
+ ut = (ARGV[0].to_f * (ARGV[1].to_f + (ARGV[2].to_f + ARGV[2].to_f ) )) - ARGV[4].to_f
 
 if ut > 0
     puts ut * 0.65
