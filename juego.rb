27b28a2f62a418a3df4d5fa63ee6afe84f39709a
Pedro <pedromatu29@gmail.com>
2019-04-03 20:55:37 -0300

    beta

---
+++ juego.rb
@@ -0,0 +1,30 @@
+jugadas = ["piedra" , "papel" , "tijera"]
+
+usuario = ARGV[0]
+computador = jugadas[rand(0..2)]
+
+
+#Usuario=piedra
+if usuario == "piedra" and computador == "papel"
+    puts "¡Perdiste!, computador hizo papel"
+elsif usuario == "piedra" and computador == "piedra"
+    puts "Empate..."
+elsif usuario == "piedra" and computador == "tijera"
+    puts "Felicidades, venciste al computador"
+
+#usuario=papel    
+elsif usuario == "papel" and computador == "papel"
+    puts "Empate..."
+elsif usuario == "papel" and computador == "tijera"
+    puts "¡Perdiste!, computador hizo tijera"
+elsif usuario == "papel" and computador == "piedra"
+    puts "Felicidades, venciste al computador"
+#usuario=tijera
+elsif usuario == "tijera" and computador == "papel"
+    puts "Felicidades, venciste al computador"
+elsif usuario == "tijera" and computador == "piedra"
+    puts "¡Perdiste!, computador hizo piedra"
+elsif usuario == "tijera" and computador == "tijera"
+    puts "Empate..."
+end
+
---
+++ mayor_de_4.rb
@@ -0,0 +1,15 @@
+numero1 = ARGV[0].to_i
+numero2 = ARGV[1].to_i
+numero3 = ARGV[2].to_i
+numero4 = ARGV[3].to_i  
+
+if (numero1 >= numero2 and numero1 >= numero3 and numero1 >= numero4)
+    puts "#{numero1} es número el mayor"
+elsif (numero2 >= numero1 and numero2 >= numero3 and numero2 >= numero4)
+    puts "#{numero2} es el número mayor"
+elsif (numero3 >= numero1 and numero3 >= numero2 and numero3 >= numero4)
+    puts "#{numero3} es el número mayor"
+elsif (numero4 >= numero1 and numero4 >= numero2 and numero4 >= numero3)
+    puts "#{numero4}  es el número mayor"
+end 
+
