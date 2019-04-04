f95ce9a37678d266c2c3b78b3663d45791a7ef64
Pedro <pedromatu29@gmail.com>
2019-04-03 21:25:14 -0300

    mayor o igual

--- mayor_de_3.rb
+++ mayor_de_3.rb
@@ -2,11 +2,11 @@ numero1 = ARGV[0].to_i
 numero2 = ARGV[1].to_i
 numero3 = ARGV[2].to_i
 
-if (numero1 > numero2 and numero1 > numero3)
+if (numero1 >= numero2 and numero1 >= numero3)
     puts "#{numero1} es número el mayor"
-elsif (numero2 > numero1 and numero2 > numero3)
+elsif (numero2 >= numero1 and numero2 >= numero3)
     puts "#{numero2} es el número mayor"
-elsif (numero3 > numero1 and numero3 > numero2)
+elsif (numero3 >= numero1 and numero3 >= numero2)
     puts "#{numero3} es el número mayor"
 end 
 
